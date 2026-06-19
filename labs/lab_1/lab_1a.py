5e83346f9c3cba70c19b4396bd317bb6a9a32758
mzjohnny <mzjohnnyli114@gmail.com>
2026-06-18 22:30:42 -0500

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -12,7 +12,7 @@ def main():
     name = "Mingzhou Li"  # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-    print("Hi, I'm from Lake Forest Academy Chicago")
+    print("Hi, I'm from UHS")
 
 if __name__ == "__main__":
     main()
